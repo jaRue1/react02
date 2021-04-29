397e19e3df36a148d733db9e126b5cb0afeb384f
jaRue1 <jaruejohnson@outlook.com>
2021-04-29 16:14:57 -0400

    added a class component

--- src/components/Coffees/index.js
+++ src/components/Coffees/index.js
@@ -1,7 +1,47 @@
 import React from 'react'
-
-function Coffees(){
-  return <p>This is Coffees</p>
+//Functional Component
+// function Coffees(){
+//   return (
+//   <section>
+//     Functional Component Coffees
+//   </section>
+//   )
+// }
+// Class Component
+class Coffees extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [],
+    }
+  }
+  componentDidMount(){
+    console.log("This loaded painfully")
+    fetch('https://api.sampleapis.com/coffee/hot')
+    .then(response => console.log(response.json()))
+    .catch()
+  }
+  render() {
+    return (
+    <> 
+      <ul className="allCoffees">
+        <li>
+          <h3>Expresso</h3>
+          <span>Description</span>
+        </li>
+        <li>
+          <h3>Mocha</h3>
+          <span>Description</span>
+        </li>
+        <li>
+          <h3>Cafe</h3>
+          <span>Description</span>
+        </li>
+      </ul>
+    </>
+    )
+  }
 }
+
 
 export default Coffees
