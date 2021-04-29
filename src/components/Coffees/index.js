159db7fc490d907ed6d7b5462aa243dd6d7db780
jaRue1 <jaruejohnson@outlook.com>
2021-04-29 17:23:35 -0400

    update my class component

--- src/components/Coffees/index.js
+++ src/components/Coffees/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 //Functional Component
 // function Coffees(){
 //   return (
@@ -7,41 +7,47 @@ import React from 'react'
 //   </section>
 //   )
 // }
+
+// Path of execution
+// 1: Runs Constructor data : []
+// 2: Runs render() logs an empty array
+// 3: Runs ComponentDidMount() and gets data
+// 4: Runs render() with updated data
+
 // Class Component
 class Coffees extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       data: [],
     }
   }
-  componentDidMount(){
+  componentDidMount() {
     console.log("This loaded painfully")
-    fetch('https://api.sampleapis.com/coffee/hot')
-    .then(response => console.log(response.json()))
-    .catch()
+    fetch("https://api.sampleapis.com/coffee/hot") // get the data
+      .then((response) => response.json()) // pulls the json out of the response
+      .then((dataReceived) => this.setState({ data: dataReceived })) // then set the json data to state
+      .catch()
   }
   render() {
+    console.log("this is the data ", this.state.data)
     return (
-    <> 
-      <ul className="allCoffees">
-        <li>
-          <h3>Expresso</h3>
-          <span>Description</span>
-        </li>
-        <li>
-          <h3>Mocha</h3>
-          <span>Description</span>
-        </li>
-        <li>
-          <h3>Cafe</h3>
-          <span>Description</span>
-        </li>
-      </ul>
-    </>
+      <>
+        <ul className="allCoffees">
+          {/* logs an individual coffee */}
+          {/* {this.state.data.map(item => console.log(item))} */}
+          {this.state.data.map((item) => {
+            return(
+            <li key={item.id}> 
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+            </li>
+            )
+          })}
+        </ul>
+      </>
     )
   }
 }
-
 
 export default Coffees
