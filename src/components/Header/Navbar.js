9da6389d2c6b67dcaca322bc875384ee9cc41f11
jaRue1 <jaruejohnson@outlook.com>
2021-04-30 09:57:05 -0400

    implemented my first routes

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,10 +1,16 @@
-import React from 'react'
-// passing props isLoggedIn from Header index.js 
-function NavBar({ isLoggedIn, setIsLoggedIn }){
-  if (!isLoggedIn) {
-    return <button onClick ={() => setIsLoggedIn(true)}>Login</button>
-  }
-  return <button onClick ={() => setIsLoggedIn(false)}>Logout</button>
+import React from "react"
+import { Link } from "react-router-dom"
+// passing props isLoggedIn from Header index.js
+function NavBar({ isLoggedIn, setIsLoggedIn }) {
+  return(
+  <nav>
+    <Link to='/'>Home</Link>
+    <Link to='/coffee'>Coffee</Link>
+    <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+      {isLoggedIn ? 'Login' : 'Logout'}
+    </button>
+  </nav>
+  )
 }
 
 export default NavBar
