2af185566525f3f3fb21bdca861d80a9ae6d1e87
jaRue1 <jaruejohnson@outlook.com>
2021-04-30 09:38:09 -0400

    added my first routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React , { useState } from 'react'
+import React, { useState } from "react"
 import "./App.css"
+// Importing react router dom, switch and route
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 //----------------- Importing My Components ----------------------
-import Header from "./components/Header/index" 
+import Header from "./components/Header/index"
 import Main from "./components/Main/index"
 import Footer from "./components/Footer/index"
 import Coffees from "./components/Coffees"
 //----------------------------------------------------------------
 
 function App() {
-  // const isLoggedIn = false 
+  // const isLoggedIn = false
   // Using State (Replaces line above)
   const [isLoggedIn, setIsLoggedIn] = useState(false) // how to create a state variable
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* Passing isLoggedIn into Header (issa Props) */}
-        <Header 
-        setIsLoggedIn= { setIsLoggedIn }
-        isLoggedIn={ isLoggedIn } 
-        />
-      </header>
-      <section className="main-section main-style">
-      <Main/>
-      </section>
-      <Coffees />
-      <Footer />
-    </div>
+    // added the ability to use the react router throughout the app
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          {/* Passing isLoggedIn into Header (issa Props) */}
+          <Header setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
+        </header>
+        <section className="main-section main-style">
+          <Switch>
+            <Route path="/coffee" component>
+              <Coffees />
+            </Route>
+            <Route path="/" component>
+              <Main />
+            </Route>
+          </Switch>
+        </section>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
